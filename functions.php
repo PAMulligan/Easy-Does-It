5308d7650ecbd8802d673c3d82905b3f5531db54
PaulDRev <paul@dentalrevenue.com>
2024-03-16 01:35:34 -0400

    Add menu editing in admin dashboard

--- functions.php
+++ functions.php
@@ -1,10 +1,19 @@
-<?
-add_action( 'wp_enqueue_scripts', 'easy_does_it_enqueue_styles' );
-
+<?php
 function easy_does_it_enqueue_styles() {
 	wp_enqueue_style( 
 		'easy-does-it-style', 
 		get_stylesheet_uri()
 	);
 }
+add_action( 'wp_enqueue_scripts', 'easy_does_it_enqueue_styles' );
+
+function register_easy_does_it_menus() {
+	register_nav_menus(
+		array(
+			'header-menu' => __( 'Header Menu' ),
+			'footer-menu' => __( 'Footer Menu' )
+		)
+	);
+}
+add_action( 'init', 'register_easy_does_it_menus' );
 ?>
