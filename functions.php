43a8a7731628b57bc4e2edf19b28f7c5c9b8a162
PaulDRev <paul@dentalrevenue.com>
2024-03-26 16:22:28 -0400

    Work on first portfolio example)

--- functions.php
+++ functions.php
@@ -1,11 +1,19 @@
 <?php
-function easy_does_it_enqueue_styles() {
-	wp_enqueue_style( 
-		'easy-does-it-style', 
+function easy_does_it_scripts() {
+	wp_enqueue_style(
+		'easy-does-it-style',
 		get_stylesheet_uri()
 	);
+	wp_enqueue_script(
+		'easy-does-it-script',
+		'./script.js',
+		array('jquery'), 1.1,
+		array(
+			'strategy' => 'defer',
+		)
+	);
 }
-add_action( 'wp_enqueue_scripts', 'easy_does_it_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'easy_does_it_scripts' );
 
 function register_easy_does_it_menus() {
 	register_nav_menus(
---
+++ script.js
@@ -0,0 +1,3 @@
+document.querySelector("body > div > header") = $("header").height();
+console.log(headerHeight);
+// $(".entry-content > div:first-child").height()
